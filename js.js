e2b662f45fffd7b4e848c787086e09be55bbedae
kyuubi-Yhp <south01village@gmail.com>
2024-11-10 22:58:53 -0800

    fix

--- js.js
+++ js.js
@@ -10,35 +10,39 @@ whatUsee()
 ownOrtwo()
 
 function ownOrtwo() {
-const choise2 = Number(prompt('теперь приступим к первуму заданию куда отправимся на пустошь или в пищеру', 'выбери 1 или 2'))
-if (choise2 === 1) {
-  const damm = confirm('вы встречаете монстра большой паук, атаковать или бежать?')
-// if (damm == true & inventory == 'Black Sword') 
-if (damm == true & inventory.includes('Black Sword')) {
-  alert('вы убили паука мечем, победа!')
-} else {
-  alert('вы погибли против пауков лучше использовать меч')
-}
-} else if (choise2 === 2) {
-  const damm2 = confirm('вы встречаете стального слизня, атаковать или бежать?')
-  if (damm2 == true & skills.includes('Bane')) {
-    alert('яд проник внутрь и разбил днк слизня, вы победили!')
-  } else {
-    alert('против слизней хорошо работают яды, но а мечи бесполезны, вы убиты')
+  const choise2 = Number(prompt('теперь приступим к первуму заданию куда отправимся на пустошь или в пищеру', 'выбери 1 или 2'))
+  if (choise2 === 1) {
+    const damm = confirm('вы встречаете монстра большой паук, атаковать или бежать?')
+    // if (damm == true & inventory == 'Black Sword') 
+    if (damm == true & inventory.includes('Black Sword')) {
+      alert('вы убили паука мечем, победа!')
+    } else if (damm == false) {
+      alert('вы ушли и возможно сохранили себе жизнь.')
+    } else {
+      alert('вы погибли против пауков лучше использовать меч')
+    }
+  } else if (choise2 === 2) {
+    const damm2 = confirm('вы встречаете стального слизня, атаковать или бежать?')
+    if (damm2 == true & skills.includes('Bane')) {
+      alert('яд проник внутрь и разбил днк слизня, вы победили!')
+    } else if (damm2 == false) {
+      alert('вы ушли и возможно сохранили себе жизнь.')
+    } else {
+      alert('против слизней хорошо работают яды, но а мечи бесполезны, вы убиты')
+    }
   }
-}
 }
 // функция меч или яд
 function whatUsee() {
-if (sumWallet() === 100) {
-  alert('отлично теперь ты видишь свои характеристики. Давай купим оружие! На твои деньги можно купить меч')
+  if (sumWallet() === 100) {
+    alert('отлично теперь ты видишь свои характеристики. Давай купим оружие! На твои деньги можно купить меч')
     console.log(`Ваш мешок златых: баланс ${sumWallet() - 100}`)
-  inventory.push('Black Sword')
-  console.log(inventory)
-} else if (lvl === 20) {
-  alert('вы получили очки опыта и достигли 1 уровня у вас есть 1 жетион НРС предлагаю вкачать способность отравление')
-  skills.push('Bane')
-}
+    inventory.push('Black Sword')
+    console.log(inventory)
+  } else if (lvl === 20) {
+    alert('вы получили очки опыта и достигли 1 уровня у вас есть 1 жетион НРС предлагаю вкачать способность отравление')
+    skills.push('Bane')
+  }
 }
 // функция развилка выбора 
 function wallAndlvl() {
@@ -67,8 +71,8 @@ function errorRecvest() {
 // функция подсчета массива wallet 
 function sumWallet() {
   let sum = 0
-  wallet.forEach(function(wall) {
-  sum = sum + wall
+  wallet.forEach(function (wall) {
+    sum = sum + wall
   })
   return sum
 }
